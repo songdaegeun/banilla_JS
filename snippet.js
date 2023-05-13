7f488ecd9cf5d03c3764175abd32f3de9829698a
sdg <eorms6199@gmail.com>
2023-05-13 22:50:53 +0900

    poema JS

--- snippet.js
+++ snippet.js
@@ -1,1 +1,16 @@
-console.log("hello, JS!");
+var key = Symbol('symbol key');
+
+const obj = {};
+obj[key] = 'value';
+obj["key2"] = 'value2';
+obj["key3"] = 'value3';
+
+// console.log(obj[key]); // value
+
+for(let i in obj) {
+	console.log(i);
+}
+
+console.log(Object.getOwnPropertyNames(obj));
+console.log(Object.getOwnPropertySymbols(obj));
+console.log(function(){})
