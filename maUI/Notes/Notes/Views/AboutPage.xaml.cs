bd56889df817ef34ac7fe5a6bb11f82b7f05da19
sdg <eorms6199@gmail.com>
2023-09-18 00:11:00 +0900

    maui blog learning set

--- maUI/Notes/Notes/Views/AboutPage.xaml.cs
+++ maUI/Notes/Notes/Views/AboutPage.xaml.cs
@@ -7,12 +7,13 @@ public partial class AboutPage : ContentPage
         InitializeComponent();
     }
 
+    // need to search about signature. 
     private async void LearnMore_Clicked(object sender, EventArgs e)
     {
-        if (BindingContext is Models.About about)
+        if (BindingContext is Models.About about2)
         {
             // Navigate to the specified URL in the system browser.
-            await Launcher.Default.OpenAsync(about.MoreInfoUrl);
+            await Launcher.Default.OpenAsync(about2.MoreInfoUrl);
         }
     }
 }
